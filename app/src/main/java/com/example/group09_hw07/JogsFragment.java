c24a972feeabf73b621ffde64443af1c2f9fc247
Sakthivel Ravichandran <sakthivelravichandran18@gmail.com>
2021-12-06 11:34:50 -0500

    sort jogs in desc order

--- app/src/main/java/com/example/group09_hw07/JogsFragment.java
+++ app/src/main/java/com/example/group09_hw07/JogsFragment.java
@@ -23,6 +23,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -136,6 +137,7 @@ public class JogsFragment extends Fragment {
         });
         mJogs.clear();
         db.collection("jogs")
+                .orderBy("createdAt", Query.Direction.DESCENDING)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value,
